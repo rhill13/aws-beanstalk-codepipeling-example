2291d0c48fba51118124ddcc745592ed9b7cc336
Ryan Hill <ryanhill1128@gmail.com>
2021-11-05 15:24:27 -0500

    initial commit

---
+++ web/src/main/java/biz/rookware/web/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package biz.rookware.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({ "", "/", "index", "index.html" })
+public class IndexController {
+
+  @GetMapping
+  public String getIndex() {
+    return "index/index";
+  }
+}
